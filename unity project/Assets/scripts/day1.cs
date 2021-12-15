0e61d798fa415a4b835f86c0b2d55d1c77129d48
Ravi Bechoe <35457924+rbechoe@users.noreply.github.com>
2021-12-15 20:30:51 +0100

    day 1 completed

---
+++ unity project/Assets/scripts/day1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class day1 : MonoBehaviour
+{
+    public string[] entries;
+
+    void Start()
+    {
+        Stopwatch st = new Stopwatch();
+        st.Start();
+
+        FileStream fs = new FileStream(Application.dataPath + "/inputs/one.txt", FileMode.Open);
+        string content = "";
+        using (StreamReader read = new StreamReader(fs, true))
+        {
+            content = read.ReadToEnd();
+        }
+        entries = Regex.Split(content, "\r\n?|\n", RegexOptions.Singleline);
+
+        // logic here
+        int count = 0;
+        // skip 0 since there is no prev data
+        for (int i = 1; i < entries.Length - 3; i++)
+        {
+            // no need to add 1 and 2 since they are the same for both
+            if (int.Parse(entries[i + 3]) > int.Parse(entries[i])) count++;
+        }
+        UnityEngine.Debug.Log("increments: " + count);
+
+        st.Stop();
+        UnityEngine.Debug.Log(string.Format("took {0} ms to complete", st.ElapsedMilliseconds));
+    }
+}
---
+++ unity project/Assets/scripts/template.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class template : MonoBehaviour
+{
+    public string[] entries;
+
+    void Start()
+    {
+        Stopwatch st = new Stopwatch();
+        st.Start();
+
+        FileStream fs = new FileStream(Application.dataPath + "/scripts/teninput.txt", FileMode.Open);
+        string content = "";
+        using (StreamReader read = new StreamReader(fs, true))
+        {
+            content = read.ReadToEnd();
+        }
+        entries = Regex.Split(content, "\r\n?|\n", RegexOptions.Singleline);
+
+        // logic here
+
+        st.Stop();
+        UnityEngine.Debug.Log(string.Format("took {0} ms to complete", st.ElapsedMilliseconds));
+    }
+}
