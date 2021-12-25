8fca775ae96b8638b46cce95c7055228a03ac13b
Ravi Bechoe <35457924+rbechoe@users.noreply.github.com>
2021-12-25 17:56:04 +0100

    did optimization and a bit of refactoring to finally get the proper answer!!

--- unity project/Assets/scripts/day21.cs
+++ unity project/Assets/scripts/day21.cs
@@ -9,194 +9,101 @@ using System;
 
 public class day21b : MonoBehaviour
 {
-    public ulong player1wins;
-    public ulong player2wins;
-    public ulong winCondition = 21;
-    public ulong totalRolls = 0;
-    public ulong unFinishedGames = 0;
+    public ulong player1wins = 0;
+    public ulong player2wins = 0;
+    public int winCondition = 21;
+    public int startP1 = 4;
+    public int startP2 = 8;
 
-    // List<ulong[]> - p1, pos1, score1, p2, pos2, score2, gamestate
-    // state 0 = ongoing, 1 = finished
-    public List<ulong[]> games = new List<ulong[]>();
+    Dictionary<int, ulong> possibleTotals = new Dictionary<int, ulong>();
 
     void Start()
     {
         Stopwatch st = new Stopwatch();
         st.Start();
-        
-        // assign starting values
-        games.Add(new ulong[] { 0, 4, 0, 1, 6, 0, 0 });
 
         // starting games
-        RollDieA();
-        RollDieB();
+        RollOutcomes();
+        PlayGame(0, 0, startP1, startP2, 1, 1);
 
-        StartCoroutine(extraBRRRR());
+        UnityEngine.Debug.Log(player1wins);
+        UnityEngine.Debug.Log(player2wins);
 
         st.Stop();
-        UnityEngine.Debug.Log(games.Count);
         UnityEngine.Debug.Log(string.Format("took {0} ms to complete", st.ElapsedMilliseconds));
-        //UnityEngine.Debug.Log("Answer: " + (totalRolls * scores[losingPlayer]));
     }
 
-    IEnumerator extraBRRRR()
+    void RollOutcomes()
     {
-        while (unFinishedGames > 0)
+        // all possible rolls per die, some totals happen more often
+        // eg 2+2+1 = 1+3+1 = 1+2+2 etc.
+        for (int die1 = 1; die1 <= 3; die1++)
         {
-            RollDieA();
-            RollDieB();
-            yield return new WaitForEndOfFrame();
+            for (int die2 = 1; die2 <= 3; die2++)
+            {
+                for (int die3 = 1; die3 <= 3; die3++)
+                {
+                    int totalRoll = die1 + die2 + die3;
+                    if (possibleTotals.ContainsKey(totalRoll))
+                    {
+                        possibleTotals[totalRoll] += 1;
+                    }
+                    else
+                    {
+                        possibleTotals.Add(totalRoll, 1);
+                    }
+                }
+            }
         }
     }
 
-    void RollDieA()
+    // recursive games
+    void PlayGame(int p1points, int p2points, int p1pos, int p2pos, int turn, ulong games)
     {
-        // die: 1 - 1 - 1 = 3
-        // die: 1 - 2 - 1 = 4
-        // die: 2 - 1 - 1 = 4
-        // die: 3 - 1 - 3 = 7
-        // die: 2 - 3 - 2 = 7
+        if (p1points > winCondition || p2points > winCondition) return;
 
-        // copy current lists before making new lists for the dies, once done apply the new lists to the existing ones
-        List<ulong[]> newGames = new List<ulong[]>();
-        ulong calc = 0;
-        unFinishedGames = 0;
-        for (ulong i = 0; i < Convert.ToUInt64(games.Count); i++)
+        if (turn == 1)
         {
-            if (games[(int)i][6] == 1) continue;
+            // player 1
+            foreach (KeyValuePair<int, ulong> roll in possibleTotals)
+            {
+                int newPoints = newPos(p1pos, roll.Key);
 
-            // does 3 new rolls per game which results in final die score being 3-9
-            // List<ulong[]> { p1, pos1, score1, p2, pos2, score2, gamestate }
-            // final die score = 3
-            calc = newVal(games[(int)i][1] + 3, 10);
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            // final die score = 4
-            calc = newVal(games[(int)i][1] + 4, 10);
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            // final die score = 5
-            calc = newVal(games[(int)i][1] + 5, 10);
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            // final die score = 6
-            calc = newVal(games[(int)i][1] + 6, 10);
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            // final die score = 7
-            calc = newVal(games[(int)i][1] + 7, 10);
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            // final die score = 8
-            calc = newVal(games[(int)i][1] + 8, 10);
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            // final die score = 9
-            calc = newVal(games[(int)i][1] + 9, 10);
-            newGames.Add(new ulong[] { 0, calc, games[(int)i][2] + calc, 1, games[(int)i][4], games[(int)i][5], 0 });
-            totalRolls += 15;
+                if (p1points + newPoints >= winCondition)
+                {
+                    // player 1 won in all games that have the same rolls
+                    player1wins += games * roll.Value;
+                }
+                else
+                {
+                    PlayGame(p1points + newPoints, p2points, newPoints, p2pos, 2, roll.Value * games);
+                }
+            }
         }
-        newGames = EvaluateGamesA(newGames);
-        games = newGames;
+        else
+        {
+            // player 2
+            foreach (KeyValuePair<int, ulong> roll in possibleTotals)
+            {
+                int newPoints = newPos(p2pos, roll.Key);
+
+                if (p2points + newPoints >= winCondition)
+                {
+                    // player 2 won in all games that have the same rolls
+                    player2wins += games * roll.Value;
+                }
+                else
+                {
+                    PlayGame(p1points, p2points + newPoints, p1pos, newPoints, 1, roll.Value * games);
+                }
+            }
+        }
     }
 
-    void RollDieB()
+    int newPos(int position, int roll)
     {
-        // die: 1 - 1 - 1 = 3
-        // die: 1 - 2 - 1 = 4
-        // die: 2 - 1 - 1 = 4
-        // die: 3 - 1 - 3 = 7
-        // die: 2 - 3 - 2 = 7
-
-        // copy current lists before making new lists for the dies, once done apply the new lists to the existing ones
-        List<ulong[]> newGames = new List<ulong[]>();
-        ulong calc = 0;
-        unFinishedGames = 0;
-        for (ulong i = 0; i < Convert.ToUInt64(games.Count); i++)
-        {
-            if (games[(int)i][6] == 1) continue;
-
-            // does 3 new rolls per game which results in final die score being 3-9
-            // List<ulong[]> { p1, pos1, score1, p2, pos2, score2, gamestate }
-            // final die score = 3
-            calc = newVal(games[(int)i][4] + 3, 10);
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            // final die score = 4
-            calc = newVal(games[(int)i][4] + 4, 10);
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            // final die score = 5
-            calc = newVal(games[(int)i][4] + 5, 10);
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            // final die score = 6
-            calc = newVal(games[(int)i][4] + 6, 10);
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            // final die score = 7
-            calc = newVal(games[(int)i][4] + 7, 10);
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            // final die score = 8
-            calc = newVal(games[(int)i][4] + 8, 10);
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            // final die score = 9
-            calc = newVal(games[(int)i][4] + 9, 10);
-            newGames.Add(new ulong[] { 0, games[(int)i][1], games[(int)i][2], 1, calc, games[(int)i][5] + calc, 0 });
-            totalRolls += 15;
-        }
-        newGames = EvaluateGamesB(newGames);
-        games = newGames;
-    }
-
-    ulong newVal(ulong val, ulong sub)
-    {
-        while(val > sub) val -= sub;
-        if (val == 0) val = 1;
+        int val = position + roll;
+        while (val > 10) val -= 10;
         return val;
-    }
-
-    // check for player 1
-    List<ulong[]> EvaluateGamesA(List<ulong[]> gamesToCheck)
-    {
-        for (ulong i = 0; i < Convert.ToUInt64(games.Count); i++)
-        {
-            if (gamesToCheck[(int)i][2] > winCondition && gamesToCheck[(int)i][6] == 0)
-            {
-                player1wins++;
-                gamesToCheck[(int)i][6] = 1;
-            }
-            else
-            {
-                unFinishedGames++;
-            }
-        }
-
-        return gamesToCheck;
-    }
-
-    // check for player 2
-    List<ulong[]> EvaluateGamesB(List<ulong[]> gamesToCheck)
-    {
-        for (ulong i = 0; i < Convert.ToUInt64(games.Count); i++)
-        {
-            if (gamesToCheck[(int)i][5] > winCondition && gamesToCheck[(int)i][6] == 0)
-            {
-                player2wins++;
-                gamesToCheck[(int)i][6] = 1;
-            }
-            else
-            {
-                unFinishedGames++;
-            }
-        }
-
-        return gamesToCheck;
     }
 }
